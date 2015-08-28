1afdb86bcc11941b5e68e695d9c81d44c17a511d
jmortl03 <jmortlock93@hotmail.com>
2015-08-28 13:14:43 +1000

    Re-attempt at merging

--- src/datamanagement/AppProperties.java
+++ src/datamanagement/AppProperties.java
@@ -1,14 +1,38 @@
 package datamanagement;
+/**
+ * importing necessary java utilities
+ */
 import java.util.*;
 import java.io.*;
+
 public class AppProperties {
 private static AppProperties self = null;
-private Properties properties;
+private Properties properties_;
 
+
+
+/**
+ * method to get instance
+ * @return  properties_
+ * @throws RuntimeException
+ */
 public static AppProperties getInstance() {
-if (self == null ) { self = new AppProperties(); } return self;}
-private AppProperties() {properties = new Properties();
-try {properties.load(new FileInputStream("Properties.prop"));} 
-catch (IOException e) {throw new RuntimeException("Could not read property file");}}
-    public Properties getProperties() {return properties;
-}}
+	if (self == null ) { self = new AppProperties(); } return self;
+	}
+	private AppProperties() {properties_ = new Properties();
+		/**
+                 * try to load in file Properties.prop, if unable, display error message
+                 * 
+                 * @param properties_
+                 * @throws RuntimeException
+                 */
+		try {properties_.load(new FileInputStream("Properties.prop"));} 
+		catch (IOException e) {throw new RuntimeException("Could not read property file");}
+	}
+	/**
+         * return the results
+         * @return properties_
+         */
+         
+	public Properties getProperties() {return properties_;}
+}
